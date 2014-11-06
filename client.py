34191ca537cc5c59c47ef2fe7f1d0e0b9b48ee5f
Priyansh Trivedi <devl.priyansh@gmail.com>
2014-11-06 17:35:19 +0530

    v0.7.5 Try and catch errors Changes 	-Implemented some basic error catching to prevent program crashes due to bad network 	-The server will try to sequentially try for ports while binding to them. And will throw the final port number at the end 	-The server will try to connect to an online website to fetch its (server's) IP address. Will display 127.0.0.1 otherwise 	-The client will try and connect to the server, if fails then will prompt for a new IP and port.

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+from socket import *
 import socket
 import string
 import sys
@@ -5,13 +6,36 @@ import sys
 s = socket.socket()
 port = 9001
 if len(sys.argv) > 1:
-	host = sys.argv[0]
+	host = sys.argv[1]
+	print host
 else:
-	host = raw_input("Pleas enter the address of the server:\t")
+	host = raw_input("Please enter the address of the server:\t")
 continue_flag = True
 
-print "**Attempting a connection"
-s.connect((host,port))
+while True:
+	try:
+		print "**Attempting a connection on", host, ":", port
+		s.connect((host,port))
+	except socket.error as error:
+		if error.errno == 111 or error.errno == 107:
+			print "(ERROR): The client is unable to connect to the server. Please ensure that the server is online and you have specified the correct address and port"
+			print "(Ping): Enter the correct IP and Port number of the server seperated by a space. Enter exit instead to terminate this application"
+			command = raw_input().lower().strip()
+			
+			if command == "exit":
+				exit()
+
+			commands = command.split()
+			if len(commands) >= 2 and commands[1].isdigit():
+				host = commands[0]
+				port = int(commands[1])
+			else:
+				print "(ERROR): Please enter a valid IP address and port number"
+				continue
+		
+		else:
+			break
+
 
 #Just have one send one receive interface
 while continue_flag:
--- server.py
+++ server.py
@@ -9,18 +9,34 @@ connection_port = 9001
 connection = socket.socket()
 host = "0.0.0.0"
 port = connection_port
-connection.bind((host,port))	
+#Try to bind the socket to the given port. Otherwise switch to the next working port
+while True:
+	try: 
+		connection.bind((host,port))
+	except socket.error as error:
+		if error.errno == 98:
+			print "(ERROR): Port", port,"already in use. Running the server on port", port + 1, "instead"
+			port += 1
+		else:
+			break
 connection.listen(5000)
 
-'''To fetch the IP address of the server'''
+#To fetch the IP address of the server, we try and instantiate a connection to a dummy ip and port, only to fetch the IP of this computer
 temporary_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-temporary_socket.connect(("8.8.8.8",80))
-address = temporary_socket.getsockname()[0]
+try:
+	temporary_socket.connect(("8.8.8.8",80))
+	address = temporary_socket.getsockname()[0]
+except socket.error as error:
+	if error.errno == 101:
+		print "(WARNING): Server offline. Only local clients can connect."
+		address = "127.0.0.1"
+	else:
+		address = temporary_socket.getsockname()[0]
 temporary_socket.close()
 
 
 while True:
-	print "(Server): Ready to accept a new connection on %s" % address
+	print "(Server): Ready to accept a new connection on %s" % address +' : '+ str(connection.getsockname()[1])
 	client, addr  = connection.accept()
 	print addr, "Connected"
 	#Now we want to thread to a new branch and continue handling the client there
